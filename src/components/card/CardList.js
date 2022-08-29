f1ebd6eba7b72a30612e6570b39f388ac18609d8
triwibow <triw3707@gmail.com>
2022-08-29 16:20:52 +0700

    add toml

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -5,15 +5,15 @@ import 'moment/locale/id';
 
 const CardList = (props) => {
     return (
-        <div className={props.column} data-cy="item-activity">
+        <div className={props.column} data-cy="activity-item">
             <div className="card-list">
                 <Link className="link" to={props.to}>
                     <div className="card-list-body">
-                        <h5 className="text-title" data-cy="title-item-activity">{props.data.title}</h5>
+                        <h5 className="text-title" data-cy="activity-item-title">{props.data.title}</h5>
                     </div>
                 </Link>
                 <div className="card-list-footer">
-                    <span className="text-date" data-cy="date-item-activity">
+                    <span className="text-date" data-cy="activity-item-date">
                         <Moment format="DD MMMM YYYY" locale="id">
                             {props.data.created_at}
                         </Moment>
