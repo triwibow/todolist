c997410b8eceabd5d41e10860bc3535e44ec93b5
triwibow <triw3707@gmail.com>
2022-08-29 21:14:14 +0700

    add modal

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -2,16 +2,27 @@ import { useNavigate } from 'react-router-dom';
 import ModalDeleteActivity from '../modal/ModalDeleteActivity';
 import Moment from 'react-moment';
 import 'moment/locale/id';
+import { API } from '../../config/api';
 
 const CardList = (props) => {
     const navigate = useNavigate();
-    const redirect = () => {
-        navigate(`/detail/${props.data.id}`);
+    const getDetail = async () => {
+        try {
+            const response = await API.get(`/activity-groups/${props.data.id}`);
+            if(response.status === 200){
+                navigate(`/detail/${props.data.id}`);
+            }
+            
+        } catch(e)
+        {
+            console.log(e);
+        }
     }
+
     return (
         <div className={props.column} data-cy="activity-item">
             <div className="card-list">
-                <div className="card-list-body" onClick={redirect}>
+                <div className="card-list-body" onClick={getDetail}>
                     <h5 className="text-title" data-cy="activity-item-title">{props.data.title}</h5>
                 </div>
                 <div className="card-list-footer">
